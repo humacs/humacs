0fbf21e45304f338f791d7d81980bb3af28a898a
Hippie Hacker <hh@ii.coop>
2020-07-28 12:07:11 +1200

    inital import of humacs doom + spacemacs configs

---
+++ .emacs-profiles.el
@@ -0,0 +1,9 @@
+(
+ ("spacemacs" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/humacs/spacemacs-config")))))
+ ("wil-spacemacs" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/humacs/wil-spacemacs-config")))))
+ ("doom" . ((user-emacs-directory . "~/humacs/doom-emacs")
+           (env . (("DOOMDIR" . "~/humacs/doom-config")))))
+ ("sachac" . ((user-emacs-directory . "~/humacs/sachac")))
+)
---
+++ install.sh
@@ -0,0 +1,3 @@
+./chemacs/install.sh
+ln -sf $(pwd)/.emacs-profiles.el ~/
+ln -sf $(pwd)/.emacs-profile ~/
