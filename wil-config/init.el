362b0bdf11cf31d689735af4e73170b684b43844
Hippie Hacker <hh@ii.coop>
2020-07-29 20:34:19 +1200

    Save slight diff between default config / wil-config

--- wil-config/init.el
+++ wil-config/init.el
@@ -518,3 +518,6 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
   )
+
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
