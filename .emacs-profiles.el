f7425aa750e0d6c5325c3300d8e3325d4cbde52b
Hippie Hacker <hh@ii.coop>
2020-07-29 20:02:24 +1200

    Simplify git modules and profiles

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,15 +1,8 @@
 (
- ("spacemacs" . ((user-emacs-directory . "~/humacs/spacemacs")
-                 (spacemacs-banner-advice .
-                                       (advice-add
-                                        'spacemacs-buffer//choose-banner
-                                        :before
-                                        (lambda ()
-                                          (defconst spacemacs-banner-directory
-                                            "~/humacs/spacemacs-config/banners" ))))
-                 (env . (("SPACEMACSDIR" . "~/humacs/spacemacs-config")))))
+ ("ii" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/humacs/ii-config")))))
  ("wil" . ((user-emacs-directory . "~/humacs/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/humacs/wil")))))
+                 (env . (("SPACEMACSDIR" . "~/humacs/wil-config")))))
  ("doom" . ((user-emacs-directory . "~/humacs/doom-emacs")
            (env . (("DOOMDIR" . "~/humacs/doom-config")))))
  ("sachac" . ((user-emacs-directory . "~/humacs/sachac")))
