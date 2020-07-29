54187196ad1c9a4119cb3af3fd6509f22a3e4abb
Hippie Hacker <hh@ii.coop>
2020-07-30 11:50:12 +1200

    Update our wil -> wilinux .emacs-profiles.el

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,8 +1,8 @@
 (
  ("ii" . ((user-emacs-directory . "~/humacs/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/humacs/ii-config")))))
- ("wil" . ((user-emacs-directory . "~/humacs/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/humacs/wil-config")))))
+ ("wilinux" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/humacs/wilinux-config")))))
  ("doom" . ((user-emacs-directory . "~/humacs/doom-emacs")
            (env . (("DOOMDIR" . "~/humacs/doom-config")))))
  ("sachac" . ((user-emacs-directory . "~/humacs/sachac")))
