8bd9340843bafb0725240ba4adf4d4e5a6bdc3db
Hippie Hacker <hh@ii.coop>
2020-07-29 19:57:35 +1200

    Rename wil-spacemacs-config -> wil

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,8 +1,15 @@
 (
  ("spacemacs" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (spacemacs-banner-advice .
+                                       (advice-add
+                                        'spacemacs-buffer//choose-banner
+                                        :before
+                                        (lambda ()
+                                          (defconst spacemacs-banner-directory
+                                            "~/humacs/spacemacs-config/banners" ))))
                  (env . (("SPACEMACSDIR" . "~/humacs/spacemacs-config")))))
- ("wil-spacemacs" . ((user-emacs-directory . "~/humacs/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/humacs/wil-spacemacs-config")))))
+ ("wil" . ((user-emacs-directory . "~/humacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/humacs/wil")))))
  ("doom" . ((user-emacs-directory . "~/humacs/doom-emacs")
            (env . (("DOOMDIR" . "~/humacs/doom-config")))))
  ("sachac" . ((user-emacs-directory . "~/humacs/sachac")))
