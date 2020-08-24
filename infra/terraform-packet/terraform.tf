d05c44d0931167e27bf6e4129e2758251e2cb224
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-08-24 16:52:35 +1200

    Add terraform for packet

---
+++ infra/terraform-packet/terraform.tf
@@ -0,0 +1,77 @@
+# Define
+# Define the compute:
+
+variable "auth_token" {
+  description = "Token for Packet API"
+}
+
+provider "packet" {
+    auth_token = var.auth_token
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+variable "region" {
+  default     = "nrt1"
+  description = "Packet region"
+}
+
+variable "project_id" {
+  description = "Packet region"
+}
+
+variable "hostname" {
+  default = "humacs"
+  description = "The hostname for the box"
+}
+
+variable "box_type" {
+  default = "c3.small.x86"
+  description = "The size of the box"
+}
+
+variable "operating_system" {
+  default = "ubuntu_20_04"
+  description = "The OS for the box"
+}
+
+data "template_file" "user_data" {
+  template = "${file("cloud-init.yml")}"
+}
+
+data "template_file" "user_data_base64" {
+  template = base64encode(data.template_file.user_data.rendered)
+}
+
+output "user_data_base64" {
+  description = "ID of the Packet box"
+  value       = data.template_file.user_data_base64
+}
+
+resource "packet_device" "box" {
+  hostname         = "${var.hostname}-${random_string.suffix.result}"
+  plan             = var.box_type
+  facilities       = [var.region]
+  operating_system = var.operating_system
+  billing_cycle    = "hourly"
+  project_id       = var.project_id
+  user_data        = data.template_file.user_data_base64.rendered
+}
+
+output "packet_device_id" {
+  description = "ID of the Packet box"
+  value       = packet_device.box.id
+}
+
+output "packet_device_state" {
+  description = "State of the Packet box"
+  value       = packet_device.box.state
+}
+
+output "packet_device_ip" {
+  description = "IP address of the Packet box"
+  value       = packet_device.box.access_public_ipv4
+}
