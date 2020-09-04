502e067ca37468fcc368da09fed54bb767d1ca83
Hippie Hacker <hh@ii.coop>
2020-09-04 23:13:15 +1200

    apisnoop.yaml values updated /home/{humacs->ii}

--- infra/cluster-api/cluster-setup.sh
+++ infra/cluster-api/cluster-setup.sh
@@ -2,20 +2,46 @@
 . vars.env
 . secrets.env # look at secrets.env.example
 set -x
-set -e
+#set -e
 clusterctl init --infrastructure=packet
 kubectl wait -n cluster-api-provider-packet-system \
-        --for=condition=ready pod \
+        --for=condition=ready \
         --selector=control-plane=controller-manager \
-        --timeout=300s
+        --timeout=300s \
+        pod
+kubectl wait -n capi-webhook-system \
+        --for=condition=Available \
+        --timeout=300s \
+        deployment/capi-kubeadm-control-plane-controller-manager
 kubectl create ns "$CLUSTER_NAME"
 clusterctl config cluster "$CLUSTER_NAME" \
            --from ./cluster-packet-template.yaml \
            -n "$CLUSTER_NAME" \
            > cluster-packet-"$CLUSTER_NAME".yaml
 kubectl -n "$CLUSTER_NAME" apply -f cluster-packet-"$CLUSTER_NAME".yaml
-# Would love to just tail these logs UNTIL the machine is active
-set +x # should allow us to control-c
-kubectl logs -n cluster-api-provider-packet-system \
-        --selector=control-plane=controller-manager \
-        -c manager -f
+sleep 15 # wait enough for the machine to exist
+kubectl wait -n $CLUSTER_NAME \
+        --for=condition=Ready \
+        --selector=cluster.x-k8s.io/cluster-name=$CLUSTER_NAME \
+        --timeout=300s \
+        machine
+UUID=$(kubectl get -n $CLUSTER_NAME \
+               --selector=cluster.x-k8s.io/cluster-name=$CLUSTER_NAME\
+               machine \
+               -o=jsonpath='{.items[0].spec.providerID}' \
+           | sed sXpacket://XX)
+echo "Type [~] [ENTER] [.] when you see Reached target Cloud-init target"
+ssh $UUID@sos.$FACILITY.packet.net
+kubectl -n "$CLUSTER_NAME" \
+        get secrets "$CLUSTER_NAME"-kubeconfig \
+        -o=jsonpath='{.data.value}' \
+    | base64 -d > ~/.kube/packet-"$CLUSTER_NAME"
+export KUBECONFIG=~/.kube/packet-"$CLUSTER_NAME"
+kubectl wait -n $CLUSTER_NAME \
+        --for=condition=ready \
+        --selector=app.kubernetes.io/name=humacs \
+        --timeout=600s pod
+kubectl exec -n $CLUSTER_NAME -ti statefulset/$CLUSTER_NAME -- attach
+# kubectl logs -n cluster-api-provider-packet-system \
+#         --selector=control-plane=controller-manager \
+#         -c manager -f
