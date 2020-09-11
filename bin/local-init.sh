d77f72df18a16d739ab0f35b84170493715522f3
Hippie Hacker <hh@ii.coop>
2020-09-11 13:10:14 +1200

    ensure local-init.sh loads correctly

--- bin/local-init.sh
+++ bin/local-init.sh
@@ -2,9 +2,14 @@
 
 export TMATE_SOCKET="${TMATE_SOCKET:-/tmp/ii.default.target.iisocket}"
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-(. $THISDIR/simple-init.sh) &
-until tmate -S $TMATE_SOCKET wait-for tmate-ready; do
+(. $THISDIR/simple-init.sh 2>&1 > ~/humacs.log) &
+echo -n "Humacs starting:"
+# Sleep a second while we wait for the client-attched hook to exist
+until tmate -S $TMATE_SOCKET show-hooks -g 2>&1 | grep new-window\ osc52 2>&1 >/dev/null ; do
     echo -n "."
     sleep 1
 done
+echo ""
+echo "Launching Humacs"
+sleep 2
 tmate -S $TMATE_SOCKET attach
