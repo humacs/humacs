b722360cfc7f017ef6c248c2cd9819c70fe40818
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-04-29 09:15:23 +1200

    Add usage instructions to bin/ssh-agent-export.sh

--- bin/ssh-agent-export.sh
+++ bin/ssh-agent-export.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [ -n "$(find /tmp -maxdepth 1 -name 'ssh-*' -print -quit)" ] ; then
+if [ $_ = $0 ]; then
+    echo "Usage:
+    . /usr/local/bin/ssh-agent-export.sh"
+    exit
+fi
+
+if [ -n "$(find /tmp -maxdepth 1 -name 'ssh-*' -print -quit)" ] ; then    
     sudo chgrp -R users /tmp/ssh-*
     sudo chmod -R 0770 /tmp/ssh-*
     export SSH_AUTH_SOCK=$(find /tmp /run/host/tmp/ -type s -regex '.*/ssh-.*/agent..*$' 2> /dev/null | tail -n 1)
