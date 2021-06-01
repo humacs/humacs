241bc5e653e1e51deb3393b1656be83ea19ce056
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-06-02 11:44:19 +1200

    Add port disabling in k8s svc ing reconciler

--- bin/k8s-service-ingress-port-bind-reconciler.sh
+++ bin/k8s-service-ingress-port-bind-reconciler.sh
@@ -21,6 +21,11 @@ while true; do
             continue
         fi
         overrideHost=$(cat /proc/$pid/environ | tr '\0' '\n' | grep SHARINGIO_PAIR_SET_HOSTNAME | cut -d '=' -f2)
+        disabledPorts=$(cat /proc/$pid/environ | tr '\0' '\n' | grep SHARINGIO_PAIR_INGRESS_RECONCILER_DISABLED_PORTS | cut -d '=' -f2)
+        if [ -n "$disabledPorts" ] && echo ${disabledPorts[*]} | grep -q -E "(^|\(| )$portNumber( |\)|$)"; then
+            echo "Skipping '$processName' (pid $pid) since port '$portNumber' is disabled in env"
+            continue
+        fi
         export name=$processName
         if [ -n "$overrideHost" ]; then
           export name=$overrideHost
