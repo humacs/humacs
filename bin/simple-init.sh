a025cc76aa121fce3af1483c03bad1982adae707
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-06-24 09:24:58 +1200

    Update bin/simple-init.sh (formatting and entrypoint)

--- bin/simple-init.sh
+++ bin/simple-init.sh
@@ -7,7 +7,7 @@ cd "$HOME"
 
 export TMATE_SOCKET="${TMATE_SOCKET:-/tmp/ii.default.target.iisocket}"
 export TMATE_SOCKET_NAME=`basename ${TMATE_SOCKET}`
-if tmate -S $TMATE_SOCKET wait-for tmate-ready 2> /dev/null; then
+if tmate -S "${TMATE_SOCKET}" wait-for tmate-ready 2> /dev/null; then
     set +x
     echo "Already initialised with tmate ready."
     echo "Use: attach"
@@ -21,14 +21,14 @@ then
     ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""
 fi
 
-chmod 0600 $HOME/.kube/config
+chmod 0600 "${HOME}"/.kube/config
 # If we are in cluster, set our default namespace
 SERVICE_ACCOUNT_DIR=/var/run/secrets/kubernetes.io/serviceaccount
-if [ -d $SERVICE_ACCOUNT_DIR ]; then
+if [ -d "${SERVICE_ACCOUNT_DIR}" ]; then
     export IN_CLUSTER=true
     # Ensure that the Humacs Pod's namespace is defaulted to in the local kubeconfig
     kubectl config set-context $(kubectl config current-context) \
-            --namespace=$(cat $SERVICE_ACCOUNT_DIR/namespace)
+            --namespace=$(cat "${SERVICE_ACCOUNT_DIR}"/namespace)
 else
     export IN_CLUSTER=false
 fi
@@ -37,7 +37,7 @@ fi
 (
     cd /etc/service
     for SVC in $(find . -type f -name 'run' | xargs -I {} dirname {}); do
-        mkdir $SVC/supervise
+        mkdir "${SVC}"/supervise
     done
 )
 runsvdir /etc/service &
@@ -69,20 +69,20 @@ _= . /usr/local/bin/ssh-agent-export.sh
 # Ensure that the home folder has been repopulated after a PVC recreates it
 if [ "$REINIT_HOME_FOLDER" = "true" ]; then
     (
-        if [ ! -f $HOME/.humacs-has-reinit ]; then
+        if [ ! -f "${HOME}"/.humacs-has-reinit ]; then
             cd /etc/skel
             cp -r . /home/ii
-            chmod 0600 $HOME/.kube/config
-            touch $HOME/.humacs-has-reinit
+            chmod 0600 "${HOME}"/.kube/config
+            touch "${HOME}"/.humacs-has-reinit
         fi
     )
 fi
 
 # Clone all projects
 (
-    if [ ! -z "$INIT_DEFAULT_REPOS" ]; then
-        mkdir -p $INIT_DEFAULT_REPOS_FOLDER
-        for repo in $INIT_DEFAULT_REPOS; do
+    if [ -n "$INIT_DEFAULT_REPOS" ]; then
+        mkdir -p "${INIT_DEFAULT_REPOS_FOLDER}"
+        for repo in ${INIT_DEFAULT_REPOS}; do
             if [ "$PROJECT_CLONE_STRUCTURE" = "structured" ]; then
                 git-clone-structured "$repo"
             elif [ "$PROJECT_CLONE_STRUCTURE" = "plain" ]; then
@@ -98,14 +98,14 @@ fi
 # call osc52-tmate.sh to set the ssh/web uri for this session via osc52
 # We need to wait's until the socket exists, and tmate is ready for commands
 # before doing so. (Would be easier if this were a config option for .tmate.conf)
-cd $INIT_DEFAULT_DIR
+cd "${INIT_DEFAULT_DIR}"
 (
     /usr/local/bin/tmate-wait-for-socket.sh
-    tmate -S $TMATE_SOCKET set-hook -ug client-attached # unset
-    tmate -S $TMATE_SOCKET set-hook -g client-attached 'run-shell "tmate new-window osc52-tmate.sh"'
+    tmate -S "${TMATE_SOCKET}" set-hook -ug client-attached # unset
+    tmate -S "${TMATE_SOCKET}" set-hook -g client-attached 'run-shell "tmate new-window osc52-tmate.sh"'
 )&
 
 # This is our primary background process for humacs
 # a tmate session in foreground mode, respawning if it dies
 # A default directory and org file are used to start emacsclient as the main window
-tmate -F -v -S $TMATE_SOCKET new-session -d -c $INIT_DEFAULT_DIR emacsclient --tty $INIT_ORG_FILE
+tmate -F -v -S "${TMATE_SOCKET}" new-session -d -c "${INIT_DEFAULT_DIR}" emacs -nw "${INIT_ORG_FILE}"
